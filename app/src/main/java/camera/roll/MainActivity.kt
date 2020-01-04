de5fed4cb7086df4d924a269d8bf96c44a8012bf
Ilya Saunkin <saunkin@amazon.com>
2020-01-04 17:05:29 +0300

    Add internet permission validation

--- app/src/main/java/camera/roll/MainActivity.kt
+++ app/src/main/java/camera/roll/MainActivity.kt
@@ -1,12 +1,39 @@
 package camera.roll
 
-import androidx.appcompat.app.AppCompatActivity
+import RandomuserApiRequestHandler
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import camera.roll.permissions.ensurePermission
+import camera.roll.permissions.requestPermissionsHandler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ensurePermission(permission = Manifest.permission.INTERNET, activity = this) {
+            Log.d(TAG, "Callback with permission")
+        }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        requestPermissionsHandler(
+            requestCode = requestCode,
+            permissions = permissions,
+            grantResults = grantResults
+        )
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
 }
